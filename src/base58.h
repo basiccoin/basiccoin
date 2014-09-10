33d82abec35aec58a4303332ea87c76ddcb6f8aa
ethought <looploop>
2014-09-10 18:34:52 +0930

    corrected base58

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 10, // Basiccoin addresses start with B
+        PUBKEY_ADDRESS = 85, // Basiccoin addresses start with B
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
--- src/main.cpp
+++ src/main.cpp
@@ -2683,7 +2683,9 @@ bool InitBlockIndex() {
         txNew.vout.resize(1);
         txNew.vin[0].scriptSig = CScript() << 486604799 << CBigNum(4) << vector<unsigned char>((const unsigned char*) pszTimestamp, (const unsigned char*) pszTimestamp + strlen(pszTimestamp));
         txNew.vout[0].nValue = 20000 * COIN;
-        txNew.vout[0].scriptPubKey = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+        //txNew.vout[0].scriptPubKey = CScript() << ParseHex("040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9") << OP_CHECKSIG;
+        txNew.vout[0].scriptPubKey = CScript() << ParseHex("03b5d38a1e262c21922f2ffa48375eb583eeb3e45c24e6fb367167e11eff43f80f") << OP_CHECKSIG;
+
         CBlock block;
         block.vtx.push_back(txNew);
         block.hashPrevBlock = 0;
